c0560fe1fde936b801b8cfffba3f475942ebb286
Karthik-TF <karthik.s@technoforte.co.in>
2024-09-23 12:39:16 +0530

    regex

--- registration/registration-services/src/main/java/io/mosip/registration/util/common/NinValidator.java
+++ registration/registration-services/src/main/java/io/mosip/registration/util/common/NinValidator.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
  */
 @Component
 public class NinValidator {
-	@Value("${mosip.registration.util.common.nin.regex}")
+	@Value("${mosip.registration.util.common.nin.regex:^[a-zA-Z0-9]{14,14}}")
 	private String regexPattern;
 	public boolean validate(String input) {
         Pattern pattern = Pattern.compile(regexPattern);
