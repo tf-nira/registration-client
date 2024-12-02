78f9a7c29cd4f2cead28e232532cbc056a8b20b1
manoj-7899 <manoj.n@technoforte.co.in>
2024-12-02 12:02:39 +0530

    nira payment statusCode change.

--- registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
@@ -33,6 +33,7 @@ import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
@@ -167,6 +168,9 @@ public class GenericController extends BaseController {
 
 	@Autowired
 	private PrnService prnService;
+	
+	@Value("${nira.payment.gateway.statusCode}")
+	private String statusCode;
 	
 	private boolean isPrnValid = false;
 
@@ -937,7 +941,7 @@ public class GenericController extends BaseController {
 				
 				 // Check if the field belongs to the "Notification of Change" group
 	            if ("Notification of Change".equalsIgnoreCase(field.getAlignmentGroup())) {
-	                isNotificationOfChangePresent = true;          // Found relevant group fields on this screen
+	                isNotificationOfChangePresent = true;		// Found relevant group fields on this screen
 	                FxControl control = getFxControl(field.getId());
 	                if (control != null) {
 	                    Object fieldValue = control.getData(); // Fetch the data
@@ -1189,7 +1193,7 @@ public class GenericController extends BaseController {
 	    CheckPRNStatusResponseDTO responseDTO = prnService.checkPRNStatus(prnText);
 
 	    if (responseDTO != null) {
-	        if (responseDTO.getStatusCode().equalsIgnoreCase("T")) {
+	        if (responseDTO.getStatusCode().equalsIgnoreCase(statusCode)) {
 	            if (responseDTO.getProcessFlow().equalsIgnoreCase(processFlow)) {
 	                Boolean prnCheck = checkPrnInTranscLogs(prnText, regId).isValid();
 	                if (prnCheck == null) {
