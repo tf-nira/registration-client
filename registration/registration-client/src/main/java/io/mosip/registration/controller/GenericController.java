9c3cef88df0d914cdeb666c05fc78f3c82729b06
JiveshBytes <kjivesh586@gmail.com>
2024-12-04 07:18:37 +0530

    setting null for demographic (removing hard coding)

--- registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
@@ -1532,7 +1532,7 @@ public class GenericController extends BaseController {
 								fxControl.clearValue();
 								break;
 							default:
-								if(!field.isSetRequired() && screenDTO.getOrder() == 2){
+								if(!field.isSetRequired()){
 									fxControl.selectAndSet(null);
 									fxControl.setData(null);
 									fxControl.clearToolTipText();
