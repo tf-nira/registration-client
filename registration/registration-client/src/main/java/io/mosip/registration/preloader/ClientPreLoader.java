4aca94bed45c27b9237b9e8ddc2d8844c7962827
Manishch22 <126442522+Manishch22@users.noreply.github.com>
2025-04-10 18:35:20 +0530

    Added code for bio sdk version check

--- registration/registration-client/src/main/java/io/mosip/registration/preloader/ClientPreLoader.java
+++ registration/registration-client/src/main/java/io/mosip/registration/preloader/ClientPreLoader.java
@@ -124,6 +124,12 @@ public class ClientPreLoader extends Preloader {
                 }
                 else
                     errorsFound = clientSetupValidator.isValidationFailed();
+                
+                clientSetupValidator.validateBioSDK();
+                if(clientSetupValidator.isBioSDK_updated()) {
+                	restartRequired = true;
+                    throw new RegBaseCheckedException("","Latest bio SDK downloaded, Kindly restart the client");
+                }
 
             } catch (RegBaseCheckedException e) {
                 errorsFound = true;
--- registration/registration-services/src/main/java/io/mosip/registration/update/ClientSetupValidator.java
+++ registration/registration-services/src/main/java/io/mosip/registration/update/ClientSetupValidator.java
@@ -5,9 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class ClientSetupValidator {
 
@@ -19,15 +22,22 @@ public class ClientSetupValidator {
     private static final String SLASH = "/";
 
     private static String serverRegClientURL = null;
+    private static String serverSDKManifestUrl = null;
+    private static String serverSDKZipUrl = null;
+    private static String sdkZipExtractionPath = null;
+    private static String localSDKManifestPath = null;
     private static String latestVersion = null;
     private static Manifest localManifest = null;
     private static Manifest serverManifest = null;
+    private static Manifest localSDKManifest = null;
+    private static Manifest serverSDKManifest = null;
 
     private static String environment = null;
     private static boolean validation_failed = false;
 
     private static boolean patch_downloaded = false;
     private static boolean unknown_jars_found = false;
+    private static boolean bioSDK_updated = false;
     private static Stack<String> messages = new Stack<>();
 
 
@@ -38,9 +48,14 @@ public class ClientSetupValidator {
             logger.info("Loading {} completed", PROPERTIES_FILE);
 
             serverRegClientURL = properties.getProperty("mosip.reg.client.url");
+            serverSDKManifestUrl = properties.getProperty("mosip.bio.sdk.url");
+            serverSDKZipUrl = properties.getProperty("mosip.bio.sdk.zip.url");
+            sdkZipExtractionPath = properties.getProperty("mosip.bio.sdk.zip.extraction.path");
+            localSDKManifestPath = properties.getProperty("mosip.bio.sdk.manifest.path");
             latestVersion = properties.getProperty("mosip.reg.version");
             environment = properties.getProperty("environment");
             setLocalManifest();
+            setLocalSDKManifest();
 
             Objects.requireNonNull(serverRegClientURL, "'mosip.reg.client.url' IS NOT SET");
             Objects.requireNonNull(latestVersion, "'mosip.reg.version' IS NOT SET");
@@ -124,6 +139,103 @@ public class ClientSetupValidator {
         logger.info("Checksum validation completed validation_failed : {}, patch_downloaded : {}", validation_failed,
                 patch_downloaded);
     }
+    
+    public void validateBioSDK() {
+    	if("LOCAL".equals(environment)) {
+            logger.warn("NOTE :: IGNORING LOCAL REGISTRATION CLIENT SETUP VALIDATION AS ITS LOCAL ENVIRONMENT");
+            return;
+        }
+    	
+    	setServerSDKManifest();
+    	
+    	String serverVersion = serverSDKManifest == null ? null : serverSDKManifest.getMainAttributes().getValue(Attributes.Name.MANIFEST_VERSION);
+        String localVersion = localSDKManifest == null ? null : localSDKManifest.getMainAttributes().getValue(Attributes.Name.MANIFEST_VERSION);
+        
+        if(serverVersion != null && localVersion != null && !localVersion.equals(serverVersion)) {
+        	bioSDK_updated = true;
+        	downloadLatestSDKZip();
+        }
+    }
+    
+    private void downloadLatestSDKZip() {
+    	String url = serverSDKZipUrl;
+    	String zipFilePath = "Bio_SDK.zip";
+
+        try (InputStream in = SoftwareUpdateUtil.download(url);
+             FileOutputStream out = new FileOutputStream(zipFilePath)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+            
+            renameExistingDirectory(sdkZipExtractionPath);
+            unzip(zipFilePath, sdkZipExtractionPath);
+        } catch (IOException | RegBaseCheckedException e) {
+            logger.error("Failed to download or extract the zip file", e);
+        }
+    }
+    
+    private void renameExistingDirectory(String destDir) {
+    	File dir = new File(destDir);
+        if (dir.exists()) {
+            String timestamp = new SimpleDateFormat("ddMMyyyyHHmmss").format(new Date());
+            File newDir = new File(destDir + "_" + timestamp);
+            dir.renameTo(newDir);
+        }
+    }
+    
+    private void unzip(String zipFilePath, String destDir) throws IOException {
+        File dir = new File(destDir);
+        if (!dir.exists()) dir.mkdirs();
+        try (ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath))) {
+            ZipEntry entry = zipIn.getNextEntry();
+            String rootDir = null;
+            while (entry != null) {
+            	String entryName = entry.getName();
+                if (rootDir == null) {
+                    rootDir = entryName.split("/")[0] + "/";
+                }
+                String filePath = destDir + File.separator + entryName.replaceFirst(rootDir, "");
+                if (!entry.isDirectory()) {
+                    extractFile(zipIn, filePath);
+                } else {
+                    File dirEntry = new File(filePath);
+                    dirEntry.mkdirs();
+                }
+                zipIn.closeEntry();
+                entry = zipIn.getNextEntry();
+            }
+        }
+    }
+
+    private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath))) {
+            byte[] bytesIn = new byte[1024];
+            int read;
+            while ((read = zipIn.read(bytesIn)) != -1) {
+                bos.write(bytesIn, 0, read);
+            }
+        }
+    }
+    
+//    private void downloadLatestSDKInstaller() {
+//    	String url = "https://raw.githubusercontent.com/Manishch22/VIdExpireBatchJob/main/Installer.msi";
+//        try(InputStream in = SoftwareUpdateUtil.download(url);
+//                FileOutputStream out = new FileOutputStream("Installer.msi")) {
+//        	byte[] buffer = new byte[1024];
+//            int bytesRead;
+//            while ((bytesRead = in.read(buffer)) != -1) {
+//                out.write(buffer, 0, bytesRead);
+//            }
+//            
+//            ProcessBuilder processBuilder = new ProcessBuilder("msiexec", "/i", "Installer.msi");
+//            //ProcessBuilder processBuilder = new ProcessBuilder("msiexec", "/i", filePath, "TARGETDIR=" + installDir, "/qn");
+//            processBuilder.start();
+//        } catch (IOException | RegBaseCheckedException e) {
+//            logger.error("Failed to load server manifest file", e);
+//        }
+//    }
 
     public boolean isValidationFailed() {
         return validation_failed;
@@ -135,6 +247,10 @@ public class ClientSetupValidator {
 
     public boolean isUnknown_jars_found() {
         return unknown_jars_found;
+    }
+    
+    public boolean isBioSDK_updated() {
+        return bioSDK_updated;
     }
 
     private void setLocalManifest() throws RegBaseCheckedException {
@@ -148,6 +264,18 @@ public class ClientSetupValidator {
             throw new RegBaseCheckedException("REG-BUILD-003", "Local Manifest not found");
         }
     }
+    
+    private void setLocalSDKManifest() throws RegBaseCheckedException {
+		try {
+			File localManifestFile = new File(localSDKManifestPath + SLASH + manifestFile);
+			if (localManifestFile.exists()) {
+				localSDKManifest = new Manifest(new FileInputStream(localManifestFile));
+			}
+		} catch (IOException e) {
+			logger.error("Failed to load local SDK manifest file", e);
+			throw new RegBaseCheckedException("REG-BUILD-003", "Local SDK Manifest not found");
+		}
+	}
 
     private void setServerManifest() {
         String url = serverRegClientURL + latestVersion + SLASH + manifestFile;
@@ -157,4 +285,13 @@ public class ClientSetupValidator {
             logger.error("Failed to load server manifest file", e);
         }
     }
+    
+    private void setServerSDKManifest() {
+		String url = serverSDKManifestUrl + SLASH + manifestFile;
+		try(InputStream in = SoftwareUpdateUtil.download(url)) {
+			serverSDKManifest = new Manifest(in);
+		} catch (IOException | RegBaseCheckedException e) {
+			logger.error("Failed to load server manifest file for SDK", e);
+		}
+	}
 }
