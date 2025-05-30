d91bd18248a1aa403be99e8e52f11537d966dac5
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2025-05-30 20:17:23 +0530

    Update DocumentScanController.java

--- registration/registration-client/src/main/java/io/mosip/registration/controller/reg/DocumentScanController.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/reg/DocumentScanController.java
@@ -154,7 +154,7 @@ public class DocumentScanController extends BaseController {
 				bufferedImage = docScannerFacade.scanDocument(scanDevice, getValueFromApplicationContext(RegistrationConstants.IMAGING_DEVICE_TYPE));
 			}
 
-			if (bufferedImage == null) {
+			if (bufferedImage != null) {
 				if(subType.equals(RegistrationConstants.PROOF_OF_SIGNATURE) || subType.equals(RegistrationConstants.PROOF_OF_INTRODUCER_SIGNATURE)) {
 					LOGGER.error("Captured buffered image was null and Signature process timed out.");
 					generateAlert(RegistrationConstants.ERROR, RegistrationUIConstants.getMessageLanguageSpecific(RegistrationUIConstants.SCAN_SIGNATURE_ERROR));
