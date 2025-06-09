4477dfb4ee9dfe298a1f2f0a8caf15d3746a181d
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2025-06-09 14:39:39 +0530

    Update PacketHandlerController.java

--- registration/registration-client/src/main/java/io/mosip/registration/controller/reg/PacketHandlerController.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/reg/PacketHandlerController.java
@@ -683,9 +683,7 @@ public class PacketHandlerController extends BaseController implements Initializ
 				}
 			} catch (RuntimeException runtimeException) {
 				LOGGER.error("", runtimeException);
-			} catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+			} 
         } else {
 			if (response.getErrorResponseDTOs() != null && response.getErrorResponseDTOs().get(0).getCode()
 					.equals(RegistrationExceptionConstants.AUTH_ADVICE_USR_ERROR.getErrorCode())) {
