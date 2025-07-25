a83eefa48c7674bf514a183c81e3e1c6dae669fa
jagadeesh.t <jagadeesh.t@TECHNO-566.technoforte.co.in>
2025-07-25 17:03:14 +0530

    Updated UNP-2232

--- registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
+++ registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
@@ -256,6 +256,7 @@ public class RegistrationConstants {
 	public static final String CHILD_FOR_AGE = "child";
 	public static final String SPOUSE = "spouse";
 	public static final String REMOVE_SPOUSE = "removeSpouse";
+	public static final String DECLARANT_NATIONALITY = "declarantNationality";
 	public static final String ENROLLMENT_COUNTRY = "enrolmentCountry";
 	public static final String ENROLLMENT_DISTRICT = "applicantPlaceOfEnrolmentDistrict";
 	public static final String ENROLLMENT_COUNTY ="applicantPlaceOfEnrolmentCounty";
