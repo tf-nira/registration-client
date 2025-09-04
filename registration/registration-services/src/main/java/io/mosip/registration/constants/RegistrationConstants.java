e806667e0110af47b7f415cd13e7e4b2b6ac5d1c
jagadeesh.t <jagadeesh.t@TECHNO-566.technoforte.co.in>
2025-09-04 16:15:58 +0530

    Configured faceRaw and Added in Packet

--- registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
+++ registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
@@ -197,6 +197,10 @@ public class RegistrationConstants {
 	public static final String PWORD = "PWD";
 	public static final String OTP = "OTP";
 	public static final String FACE = "FACE";
+	public static final String FACE_RAW = "faceRaw";
+	public static final String APPLICANT_RAW = "applicantRaw";
+	public static final String INDIVIDUAL_BIOMETRICS_RAW = "individualBiometricsRaw";
+	public static final String ORIGINAL_IMAGE = "originalImage";
 	public static final String FACE_FULLFACE = "FACE_FULL FACE";
 	public static final String IRIS = "IRIS";
 
