ce2fd684eccfda4bc40e0fc924f8dbae201a40f8
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2025-12-10 19:49:33 +0530

    Update GenericController.java

--- registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
@@ -200,7 +200,10 @@ public class GenericController extends BaseController {
 	private static final List<String> familyRoles = Arrays.asList(
 			"NIN", "spouseNIN", "spouseTwoNIN", "spouseThreeNIN", "spouseFourNIN",
 			"fatherNIN", "motherNIN", "guardianNIN_AIN", "childNIN",
-			"childTwoNIN", "childThreeNIN", "childFourNIN", "childFiveNIN", "childSixNIN","introducerNIN"
+			"childTwoNIN", "childThreeNIN", "childFourNIN", "childFiveNIN", "childSixNIN","introducerNIN",
+			"secondSecondaryOwner","firstSecondaryOwner","primaryOwnerAIN","thirdSecondaryOwner",
+            "fourthSecondaryOwner","fifthSecondaryOwner","sixthSecondaryOwner","seventhSecondaryOwner",
+            "eighthSecondaryOwner","ninthSecondaryOwner"
 	);
 
 	public static Map<String, FxControl> getFxControlMap() {
@@ -236,6 +239,9 @@ public class GenericController extends BaseController {
 		updateFlowAllowedProcess.add(FlowType.RENEWAL.name());
 		updateFlowAllowedProcess.add(FlowType.FIRSTID.name());
 		updateFlowAllowedProcess.add(FlowType.ALIENNEW.name());
+		updateFlowAllowedProcess.add(FlowType.ALIENRENEWAL.name());
+		updateFlowAllowedProcess.add(FlowType.ALIENLOST.name());
+		updateFlowAllowedProcess.add(FlowType.DEACTIVATE.name());
 	}
 
 	private void fillHierarchicalLevelsByLanguage() {
@@ -664,8 +670,9 @@ public class GenericController extends BaseController {
 		Label navigationLabel = new Label();
 		navigationLabel.getStyleClass().add(NAV_LABEL_CLASS);
 		navigationLabel.setText(processSpecDto.getLabel().get(ApplicationContext.applicationLanguage()));
-		navigationLabel.prefWidthProperty().bind(navigationAnchorPane.widthProperty());
-		navigationLabel.setWrapText(true);
+		navigationLabel.setStyle("-fx-font-size: 12px;");
+	    navigationLabel.setWrapText(true);
+	    navigationLabel.setPrefWidth(150); 
 
 		navigationAnchorPane.getChildren().add(navigationLabel);
 		AnchorPane.setTopAnchor(navigationLabel, 5.0);
@@ -2146,6 +2153,7 @@ public class GenericController extends BaseController {
 	}
 
 }
+
 
 
 
