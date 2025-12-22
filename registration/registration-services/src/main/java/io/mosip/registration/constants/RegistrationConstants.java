5cc1323334b06483b50e49dc30c44cf12d5b830b
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2025-12-22 10:57:04 +0530

    Update RegistrationConstants.java

--- registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
+++ registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
@@ -336,6 +336,13 @@ public class RegistrationConstants {
 	public static final String UNABLE_SIGNATURE_ID = "applicantUnabletoSign";
 	public static final String UNABLE_INTRODUCER_SIGNATURE_ID = "introducerUnabletoSign";
 	public static final String ENROLMENT_STATUS = "enrolmentStatus";
+	public static final String BIRTH_STATUS = "applicantBirthPlace";
+	public static final String RESIDENCE_STATUS = "residenceStatus";
+	public static final String ORIGIN_STATUS = "applicantOriginPlace";
+	public static final String BIRTH_DISTRICT = "applicantPlaceOfBirthDistrict";
+	public static final String RESIDENCE_DISTRICT = "applicantPlaceOfResidenceDistrict";
+	public static final String ORIGIN_DISTRICT = "applicantPlaceOfOriginDistrict";
+	public static final String EMPLOYER_DISTRICT = "employerDistrict";
 	public static final String PRIMARY = "primary";
 	public static final String SECONDARY = "secondary";
 	public static final String DemoGraphicFieldMessageLabel = "demoGraphicFieldMessageLabel";
