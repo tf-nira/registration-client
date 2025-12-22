b2ef3b8043a2814c6fc779ae964a449eb1a1aea6
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2025-12-22 10:54:01 +0530

    Update CheckBoxFxControl.java

--- registration/registration-client/src/main/java/io/mosip/registration/util/control/impl/CheckBoxFxControl.java
+++ registration/registration-client/src/main/java/io/mosip/registration/util/control/impl/CheckBoxFxControl.java
@@ -168,6 +168,13 @@ public class CheckBoxFxControl extends FxControl {
 			if (uiFieldDTO.isSetRequired()){
 				resetValue();
 			}
+
+			if(uiFieldDTO.getId().equalsIgnoreCase("consent") && flowType.equals(FlowType.ALIENNEW)) {
+				DropDownFxControl dropDownFxControl = new DropDownFxControl();
+				dropDownFxControl.updateDistrictList(RegistrationConstants.INSIDE_UGANDA, RegistrationConstants.ENROLLMENT_DISTRICT);
+				dropDownFxControl.updateDistrictList(RegistrationConstants.INSIDE_UGANDA, RegistrationConstants.RESIDENCE_DISTRICT);
+				dropDownFxControl.updateDistrictList(RegistrationConstants.INSIDE_UGANDA, RegistrationConstants.EMPLOYER_DISTRICT);
+			}
 			
 			// handling other handlers
 			demographicChangeActionHandler.actionHandle((Pane) getNode(), node.getId(),
