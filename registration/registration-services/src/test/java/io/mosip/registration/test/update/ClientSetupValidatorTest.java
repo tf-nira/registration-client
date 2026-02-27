f49bdf52c8044eaa3114bb5917c7347368b27260
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2026-02-27 14:50:19 +0530

    Update ClientSetupValidatorTest.java

--- registration/registration-services/src/test/java/io/mosip/registration/test/update/ClientSetupValidatorTest.java
+++ registration/registration-services/src/test/java/io/mosip/registration/test/update/ClientSetupValidatorTest.java
@@ -50,6 +50,7 @@ public class ClientSetupValidatorTest {
 	private static final String CONNECTION_TIMEOUT = "mosip.registration.sw.file.download.connection.timeout";
 	private static final String READ_TIMEOUT = "mosip.registration.sw.file.download.read.timeout";
 	private static final String manifestFile = "MANIFEST.MF";
+	private StringBuilder textArea = new StringBuilder();
 
 	@Rule
 	public MockitoRule mockitoRule = MockitoJUnit.rule();
@@ -84,7 +85,7 @@ public class ClientSetupValidatorTest {
 
 	@Test
 	public void clientSetupValidatorTest() throws RegBaseCheckedException {
-		ClientSetupValidator clntSetupValidator = new ClientSetupValidator();
+		ClientSetupValidator clntSetupValidator = new ClientSetupValidator(message -> textArea.append(message).append("\n"));
 	}
 
 	@Test
@@ -102,14 +103,14 @@ public class ClientSetupValidatorTest {
 
 	@Test
 	public void validateBuildSetupEnvironmentTest() throws RegBaseCheckedException {
-		ClientSetupValidator clntSetUpValidator = new ClientSetupValidator();
+		ClientSetupValidator clntSetUpValidator = new ClientSetupValidator(message -> textArea.append(message).append("\n"));
 		ReflectionTestUtils.setField(clntSetUpValidator, "environment", "LOCAL");
 		clntSetUpValidator.validateBuildSetup();
 	}
 
 	@Test
 	public void validateBuildSetupsetServerManifestTest() throws RegBaseCheckedException {
-		ClientSetupValidator clntSetUpValidator = new ClientSetupValidator();
+		ClientSetupValidator clntSetUpValidator = new ClientSetupValidator(message -> textArea.append(message).append("\n"));
 		ReflectionTestUtils.setField(clntSetUpValidator, "serverRegClientURL",
 				"https://dev.mosip.net/registration-client");
 		ReflectionTestUtils.setField(clntSetUpValidator, "latestVersion", "");
