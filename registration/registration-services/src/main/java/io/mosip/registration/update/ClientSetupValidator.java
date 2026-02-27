a2b3c1abddfeb115666aeb47905a2a61f97b8e3f
jagadeesh.t <jagathiyagu540@gmail.com>
2026-02-27 13:35:55 +0530

    Updated bio_sdk renaming

--- registration/registration-services/src/main/java/io/mosip/registration/update/ClientSetupValidator.java
+++ registration/registration-services/src/main/java/io/mosip/registration/update/ClientSetupValidator.java
@@ -42,9 +42,11 @@ public class ClientSetupValidator {
     private static boolean unknown_jars_found = false;
     private static boolean bioSDK_updated = false;
     private static Stack<String> messages = new Stack<>();
+    private java.util.function.Consumer<String> messageCallback;
 
 
-    public ClientSetupValidator() throws RegBaseCheckedException {
+    public ClientSetupValidator(java.util.function.Consumer<String> callback) throws RegBaseCheckedException {
+    	this.messageCallback = callback;
         try (InputStream keyStream = ClientSetupValidator.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
             Properties properties = new Properties();
             properties.load(keyStream);
@@ -157,10 +159,10 @@ public class ClientSetupValidator {
     	String serverVersion = serverSDKManifest == null ? null : serverSDKManifest.getMainAttributes().getValue(Attributes.Name.MANIFEST_VERSION);
         String localVersion = localSDKManifest == null ? null : localSDKManifest.getMainAttributes().getValue(Attributes.Name.MANIFEST_VERSION);
         
-//        if(serverVersion != null && localVersion != null && !localVersion.equals(serverVersion)) {
+        if(serverVersion != null && localVersion != null && !localVersion.equals(serverVersion)) {
         	bioSDK_updated = true;
         	downloadLatestSDKZip();
-//        }
+        }
     }
     
     private void downloadLatestSDKZip() {
@@ -171,9 +173,10 @@ public class ClientSetupValidator {
              FileOutputStream out = new FileOutputStream(zipFilePath)) {
             byte[] buffer = new byte[1024];
             int bytesRead;
-//            while ((bytesRead = in.read(buffer)) != -1) {
-//                out.write(buffer, 0, bytesRead);
-//            }
+            messageCallback.accept("Downloading latest Bio SDK...");
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
 
             renameExistingDirectory(sdkZipExtractionPath);
             unzip(zipFilePath, sdkZipExtractionPath);
