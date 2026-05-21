153cb5415f292e2d4bd6719769d8e134ec058b1b
Jiveshbytes <kjivesh586@gmail.com>
2026-05-21 15:17:10 +0530

    adding deactivate print slip

--- registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
+++ registration/registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
@@ -2134,6 +2134,7 @@ public class RegistrationConstants {
 	public static final String ACKNOWLEDGEMENT_TEMPLATE = "Ack Template";
 
 	public static final String A6_ACKNOWLEDGEMENT_TEMPLATE_CODE = "reg_ack_a6slip_template_part";
+	public static final String REG_DEACTIVE_SLIP_TEMPLATE ="reg_deactive_slip_template";
 	public static final String A6_ACKNOWLEDGEMENT_TEMPLATE_CODE_OUTSIDE_UGANDA = "ack_a6slip_template_part_out";
 	public static final String RENEWAL_A6_ACKNOWLEDGEMENT_TEMPLATE_CODE = "renewal-reg_ack_a6slip_template_part";
 	public static final String RENEWAL_A6_ACKNOWLEDGEMENT_TEMPLATE_CODE_OUTSIDE_UGANDA = "renewal-ack_a6slip_template_part_out";
