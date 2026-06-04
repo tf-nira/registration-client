d133e6a4d9c42e2b18c50d35ca245da234d6aa8b
Satyam Rajkumar Singh <satyamsingh007007@gmail.com>
2026-06-04 10:57:40 +0530

    Revert back

--- registration/registration-client/src/main/java/io/mosip/registration/util/control/impl/DropDownFxControl.java
+++ registration/registration-client/src/main/java/io/mosip/registration/util/control/impl/DropDownFxControl.java
@@ -500,14 +500,7 @@ public class DropDownFxControl extends FxControl {
 				if(uiFieldDTO.getId().equalsIgnoreCase("maritalStatus")){
 					GenericController genericController = ClientApplication.getApplicationContext().getBean(GenericController.class);
 					Map<String, Object> demographics = genericController.getRegistrationDTOFromSession().getDemographics();
-					SimpleDto genderData = null;
-					if (demographics.get("gender") != null
-							&& !((ArrayList<?>) demographics.get("gender")).isEmpty()) {
-						genderData = (SimpleDto) ((ArrayList<?>) demographics.get("gender")).get(0);
-					} else if (demographics.get("genderCop") != null
-							&& !((ArrayList<?>) demographics.get("genderCop")).isEmpty()) {
-						genderData = (SimpleDto) ((ArrayList<?>) demographics.get("genderCop")).get(0);
-					}
+					SimpleDto genderData = (SimpleDto) ((ArrayList) demographics.get("gender")).get(0);
 					SimpleDto maritalStatusData = (SimpleDto) ((ArrayList) demographics.get("maritalStatus")).get(0);
 					FxControl fxControl1 =  getFxControl("numberOfOtherSpouses");
 					FxControl fxControl2 =  getFxControl("numberOfOtherSpousesAlien");
