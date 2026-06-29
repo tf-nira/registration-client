14a6691b94154e94d1fa8b6bbf35c7de0b3b9338
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2026-06-29 15:38:25 +0530

    Update PacketHandlerServiceImpl.java

--- registration/registration-services/src/main/java/io/mosip/registration/service/packet/impl/PacketHandlerServiceImpl.java
+++ registration/registration-services/src/main/java/io/mosip/registration/service/packet/impl/PacketHandlerServiceImpl.java
@@ -292,7 +292,7 @@ public class PacketHandlerServiceImpl extends BaseService implements PacketHandl
 		}
 		
 		// Add the "isCardRequired" field for Renewal flow type
-	    if (registrationDTO.getFlowType().equals(FlowType.RENEWAL) || registrationDTO.getFlowType().equals(FlowType.ALIENRENEWAL)) {
+	    if (registrationDTO.getFlowType().equals(FlowType.RENEWAL) || registrationDTO.getFlowType().equals(FlowType.ALIENRENEWAL) || registrationDTO.getFlowType().equals(FlowType.ALIENLOST)) {
 	        LOGGER.info("Setting isCardRequired field to Yes for Renewal flow type");
 	        registrationDTO.addDemographicField("isCardRequired", "Yes");
 	    }
