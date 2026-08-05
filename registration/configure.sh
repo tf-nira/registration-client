1a33b429fadc15cd49e7dfa9f08c4a56f36f1810
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2026-08-05 16:12:01 +0530

    Update configure.sh

--- registration/configure.sh
+++ registration/configure.sh
@@ -30,6 +30,11 @@ echo "mosip.bio.sdk.url=https://github.com/tf-nira/registration-client/releases/
 echo "mosip.bio.sdk.zip.url=https://github.com/tf-nira/registration-client/releases/download/1.2.0/Tech5.zip" >> "${work_dir}"/mosip-application.properties
 echo "mosip.bio.sdk.zip.extraction.path=C:/Tech5" >> "${work_dir}"/mosip-application.properties
 echo "mosip.bio.sdk.manifest.path=C:/Tech5/T5OmniMatchBioSDK" >> "${work_dir}"/mosip-application.properties
+echo "mosip.download.bio.sdk.url=https://${host_name}/v1/masterdata/download/bio-sdk" >> "${work_dir}"/mosip-application.properties
+echo "mosip.bio.sdk.auth.secretKey=${admin_client_secret}"  >> "${work_dir}"/mosip-application.properties
+echo "mosip.bio.sdk.auth.appId=admin" >> "${work_dir}"/mosip-application.properties
+echo "mosip.bio.sdk.auth.clientId=mosip-admin-client" >> "${work_dir}"/mosip-application.properties
+echo "mosip.bio.sdk.auth.url=https://${host_name}/v1/authmanager/authenticate/clientidsecretkey" >> "${work_dir}"/mosip-application.properties
 
 echo "jar signing keystore_secret"
 echo ${keystore_secret}
